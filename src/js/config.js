8bf39a4ce5fdca454920580015ccd1b4e362d787
suryaguthikonda <suryaguthikonda@springml.com>
2022-02-17 09:34:20 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c52c0fa0-2e12-4f48-86a9-76d2a017b8a6';
+export const MODAL_CLOSE_SEC = 2.5;
